3c1ef6e15e6de11a0a4080dbf9cad68410cefabd
Ramiro Guzman <rami.guzmi@gmail.com>
2023-06-12 23:08:33 -0300

    Arreglo Extension de los metodos

--- Assets/Scripts/Reflection/NetSerializationExtensions.cs
+++ Assets/Scripts/Reflection/NetSerializationExtensions.cs
@@ -13,10 +13,9 @@ public static class NetSerializationExtensions
 
         output.AddRange(BitConverter.GetBytes(intValue));
 
-        //La cola qe nunca va a existir.
-
         return output;
     }
+
     public static List<byte> ToMsg(this bool boolValue, char[] fieldName)
     {
         List<byte> output = new List<byte>();
@@ -27,6 +26,7 @@ public static class NetSerializationExtensions
 
         return output;
     }
+
     public static List<byte> ToMsg(this char charValue, char[] fieldName)
     {
         List<byte> output = new List<byte>();
@@ -37,10 +37,10 @@ public static class NetSerializationExtensions
 
         return output;
     }
+
     public static List<byte> ToMsg(this string stringValue, char[] fieldName)
     {
         List<byte> output = new List<byte>();
-
 
         AddHeaderMessage(output, fieldName);
 
@@ -51,33 +51,36 @@ public static class NetSerializationExtensions
 
         return output;
     }
-    public static List<byte> ToMsg(this Vector3 Vector3Value, char[] fieldName)
+
+    public static List<byte> ToMsg(this Vector3 vector3Value, char[] fieldName)
     {
         List<byte> output = new List<byte>();
 
         AddHeaderMessage(output, fieldName);
 
-        output.AddRange(BitConverter.GetBytes(Vector3Value.x));
-        output.AddRange(BitConverter.GetBytes(Vector3Value.y));
-        output.AddRange(BitConverter.GetBytes(Vector3Value.z));
+        output.AddRange(BitConverter.GetBytes(vector3Value.x));
+        output.AddRange(BitConverter.GetBytes(vector3Value.y));
+        output.AddRange(BitConverter.GetBytes(vector3Value.z));
 
         return output;
     }
-    public static List<byte> ToMsg(this Vector2 Vector2Value, char[] fieldName)
+
+    public static List<byte> ToMsg(this Vector2 vector2Value, char[] fieldName)
     {
         List<byte> output = new List<byte>();
 
         AddHeaderMessage(output, fieldName);
 
-        output.AddRange(BitConverter.GetBytes(Vector2Value.x));
-        output.AddRange(BitConverter.GetBytes(Vector2Value.y));
+        output.AddRange(BitConverter.GetBytes(vector2Value.x));
+        output.AddRange(BitConverter.GetBytes(vector2Value.y));
 
         return output;
     }
+
     public static List<byte> ToMsg(this Quaternion quaternionValue, char[] fieldName)
     {
         List<byte> output = new List<byte>();
-        
+
         AddHeaderMessage(output, fieldName);
 
         output.AddRange(BitConverter.GetBytes(quaternionValue.x));
@@ -85,9 +88,9 @@ public static class NetSerializationExtensions
         output.AddRange(BitConverter.GetBytes(quaternionValue.z));
         output.AddRange(BitConverter.GetBytes(quaternionValue.w));
 
-
         return output;
     }
+
     public static List<byte> ToMsg(this Transform transformValue, char[] fieldName)
     {
         List<byte> output = new List<byte>();
@@ -109,9 +112,12 @@ public static class NetSerializationExtensions
 
         return output;
     }
+
     public static List<byte> ToMsg(this Color colorValue, char[] fieldName)
     {
         List<byte> output = new List<byte>();
+
+        AddHeaderMessage(output, fieldName);
 
         output.AddRange(BitConverter.GetBytes(colorValue.r));
         output.AddRange(BitConverter.GetBytes(colorValue.g));
@@ -123,39 +129,31 @@ public static class NetSerializationExtensions
 
     public static int ToInt(this byte[] arrayByte)
     {
-        int fieldNameLength;
-        fieldNameLength = BitConverter.ToInt32(arrayByte, 8);
-
+        int fieldNameLength = BitConverter.ToInt32(arrayByte, 8);
         int value = BitConverter.ToInt32(arrayByte, 3 * sizeof(int) + fieldNameLength * sizeof(char));
-        
+
         return value;
     }
 
     public static char ToChar(this byte[] arrayByte)
     {
-        int fieldNameLength;
-        fieldNameLength = BitConverter.ToInt32(arrayByte, 8);
-
+        int fieldNameLength = BitConverter.ToInt32(arrayByte, 8);
         char value = BitConverter.ToChar(arrayByte, 3 * sizeof(int) + fieldNameLength * sizeof(char));
 
         return value;
     }
-    
+
     public static float ToFloat(this byte[] arrayByte)
     {
-        int fieldNameLength;
-        fieldNameLength = BitConverter.ToInt16(arrayByte, 8);
-
+        int fieldNameLength = BitConverter.ToInt32(arrayByte, 8);
         float value = BitConverter.ToSingle(arrayByte, 3 * sizeof(int) + fieldNameLength * sizeof(char));
 
         return value;
     }
-    
+
     public static bool ToBool(this byte[] arrayByte)
     {
-        int fieldNameLength;
-        fieldNameLength = BitConverter.ToInt16(arrayByte, 8);
-
+        int fieldNameLength = BitConverter.ToInt32(arrayByte, 8);
         bool value = BitConverter.ToBoolean(arrayByte, 3 * sizeof(int) + fieldNameLength * sizeof(char));
 
         return value;
@@ -163,9 +161,7 @@ public static class NetSerializationExtensions
 
     public static string ToExtendedString(this byte[] arrayByte)
     {
-        int fieldNameLength;
-        fieldNameLength = BitConverter.ToInt16(arrayByte, 8);
-
+        int fieldNameLength = BitConverter.ToInt32(arrayByte, 8);
         string value = BitConverter.ToString(arrayByte, 3 * sizeof(int) + fieldNameLength * sizeof(char));
 
         return value;
@@ -173,100 +169,89 @@ public static class NetSerializationExtensions
 
     public static Vector2 ToVector2(this byte[] arrayByte)
     {
-        int fieldNameLength;
-        fieldNameLength = BitConverter.ToInt32(arrayByte, 8);
-
+        int fieldNameLength = BitConverter.ToInt32(arrayByte, 8);
         int positionToRead = 3 * sizeof(int) + fieldNameLength * sizeof(char);
 
-        Vector2 value = new Vector2(BitConverter.ToInt32(arrayByte, positionToRead), 
-                                    BitConverter.ToInt32(arrayByte, positionToRead + sizeof(int)));
+        float x = BitConverter.ToSingle(arrayByte, positionToRead);
+        float y = BitConverter.ToSingle(arrayByte, positionToRead + sizeof(float));
+
+        Vector2 value = new Vector2(x, y);
 
         return value;
     }
 
     public static Vector3 ToVector3(this byte[] arrayByte)
     {
-        int fieldNameLength;
-        fieldNameLength = BitConverter.ToInt32(arrayByte, 8);
-
+        int fieldNameLength = BitConverter.ToInt32(arrayByte, 8);
         int positionToRead = 3 * sizeof(int) + fieldNameLength * sizeof(char);
 
-        Vector3 value = new Vector3(BitConverter.ToInt32(arrayByte, positionToRead),
-                                    BitConverter.ToInt32(arrayByte, positionToRead + sizeof(int)),
-                                    BitConverter.ToInt32(arrayByte, positionToRead + 2 * sizeof(int)));
+        float x = BitConverter.ToSingle(arrayByte, positionToRead);
+        float y = BitConverter.ToSingle(arrayByte, positionToRead + sizeof(float));
+        float z = BitConverter.ToSingle(arrayByte, positionToRead + 2 * sizeof(float));
+
+        Vector3 value = new Vector3(x, y, z);
 
         return value;
     }
+
     public static Quaternion ToQuaternion(this byte[] arrayByte)
     {
-        int fieldNameLength;
-        fieldNameLength = BitConverter.ToInt32(arrayByte, 8);
-
+        int fieldNameLength = BitConverter.ToInt32(arrayByte, 8);
         int positionToRead = 3 * sizeof(int) + fieldNameLength * sizeof(char);
 
-        Quaternion value = new Quaternion(
-                               BitConverter.ToSingle(arrayByte, positionToRead),
-                               BitConverter.ToSingle(arrayByte, positionToRead + sizeof(int)),
-                               BitConverter.ToSingle(arrayByte, positionToRead + 2 * sizeof(int)),
-                               BitConverter.ToSingle(arrayByte, positionToRead) + 3 *sizeof(int));
+        float x = BitConverter.ToSingle(arrayByte, positionToRead);
+        float y = BitConverter.ToSingle(arrayByte, positionToRead + sizeof(float));
+        float z = BitConverter.ToSingle(arrayByte, positionToRead + 2 * sizeof(float));
+        float w = BitConverter.ToSingle(arrayByte, positionToRead + 3 * sizeof(float));
+
+        Quaternion value = new Quaternion(x, y, z, w);
 
         return value;
     }
 
     public static Color ToColor(this byte[] arrayByte)
     {
-        int fieldNameLength;
-        fieldNameLength = BitConverter.ToInt32(arrayByte, 8);
-
+        int fieldNameLength = BitConverter.ToInt32(arrayByte, 8);
         int positionToRead = 3 * sizeof(int) + fieldNameLength * sizeof(char);
 
-        Color value = new Color(
-                               BitConverter.ToSingle(arrayByte, positionToRead),
-                               BitConverter.ToSingle(arrayByte, positionToRead + sizeof(int)),
-                               BitConverter.ToSingle(arrayByte, positionToRead + 2 * sizeof(int)),
-                               BitConverter.ToSingle(arrayByte, positionToRead) + 3 * sizeof(int));
+        float r = BitConverter.ToSingle(arrayByte, positionToRead);
+        float g = BitConverter.ToSingle(arrayByte, positionToRead + sizeof(float));
+        float b = BitConverter.ToSingle(arrayByte, positionToRead + 2 * sizeof(float));
+        float a = BitConverter.ToSingle(arrayByte, positionToRead + 3 * sizeof(float));
+
+        Color value = new Color(r, g, b, a);
 
         return value;
     }
 
-    public static Transform ToTransform(this byte[] arrayByte)
+    public static void SetTransformFromBytes(this Transform transform, byte[] byteArray)
     {
-        int fieldNameLength;
-        fieldNameLength = BitConverter.ToInt32(arrayByte, 8);
-        int positionToRead = 3 * sizeof(int) + fieldNameLength * sizeof(char);
+        int positionToRead = 0;
 
-
-        Vector3 position = new Vector3(
-            BitConverter.ToSingle(arrayByte, positionToRead),
-            BitConverter.ToSingle(arrayByte, positionToRead + sizeof(float)),
-            BitConverter.ToSingle(arrayByte, positionToRead + 2 * sizeof(float)));
+        float posX = BitConverter.ToSingle(byteArray, positionToRead);
+        float posY = BitConverter.ToSingle(byteArray, positionToRead + sizeof(float));
+        float posZ = BitConverter.ToSingle(byteArray, positionToRead + 2 * sizeof(float));
+        Vector3 position = new Vector3(posX, posY, posZ);
 
         positionToRead += 3 * sizeof(float);
 
-        Quaternion rotation = new Quaternion(
-            BitConverter.ToSingle(arrayByte, positionToRead),
-            BitConverter.ToSingle(arrayByte, positionToRead + sizeof(float)),
-            BitConverter.ToSingle(arrayByte, positionToRead + 2 * sizeof(float)),
-            BitConverter.ToSingle(arrayByte, positionToRead + 3 * sizeof(float)));
+        float rotX = BitConverter.ToSingle(byteArray, positionToRead);
+        float rotY = BitConverter.ToSingle(byteArray, positionToRead + sizeof(float));
+        float rotZ = BitConverter.ToSingle(byteArray, positionToRead + 2 * sizeof(float));
+        float rotW = BitConverter.ToSingle(byteArray, positionToRead + 3 * sizeof(float));
+        Quaternion rotation = new Quaternion(rotX, rotY, rotZ, rotW);
 
         positionToRead += 4 * sizeof(float);
 
-        Vector3 scale = new Vector3(
-            BitConverter.ToSingle(arrayByte, positionToRead),
-            BitConverter.ToSingle(arrayByte, positionToRead + sizeof(float)),
-            BitConverter.ToSingle(arrayByte, positionToRead + 2 * sizeof(float)));
+        float scaleX = BitConverter.ToSingle(byteArray, positionToRead);
+        float scaleY = BitConverter.ToSingle(byteArray, positionToRead + sizeof(float));
+        float scaleZ = BitConverter.ToSingle(byteArray, positionToRead + 2 * sizeof(float));
+        Vector3 scale = new Vector3(scaleX, scaleY, scaleZ);
 
-        Transform value;
-
-        value.position = position;
-        value.rotation = rotation;
-        value.localScale = scale;
-
-        return value;
+        transform.position = position;
+        transform.rotation = rotation;
+        transform.localScale = scale;
     }
-}
-
-
 
     static void AddHeaderMessage(List<byte> output, char[] fieldName)
     {
@@ -281,6 +266,3 @@ public static class NetSerializationExtensions
         }
     }
 }
-
-
-
